5e4ab31c7aeae1222c711547c153f0b32cef5445
Laurcons <laurcons@outlook.com>
2026-05-30 15:12:06 +0300

    fix: bump year in copyright notice

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ArhivUtility")]
-[assembly: AssemblyCopyright("Pricop Laurentiu, 2020-2025")]
+[assembly: AssemblyCopyright("Pricop Laurentiu, 2020-2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
