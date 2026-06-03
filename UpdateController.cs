6cf576ae86747643e3f9da90b3ac0f93f82bfac1
Laurcons <laurcons@outlook.com>
2026-06-03 22:41:48 +0300

    fix: prevent autoupdater loops by disallowing updates if the executable is not named properly

--- UpdateController.cs
+++ UpdateController.cs
@@ -1,9 +1,21 @@
-﻿using AutoUpdaterDotNET;
+﻿using System.IO;
+using System.Windows.Forms;
+using AutoUpdaterDotNET;
 
 namespace ArhivUtility {
   internal static class UpdateController {
 
     public static void PerformUpdates() {
+      if (Path.GetFileName(Application.ExecutablePath) != "ArhivUtility.exe") {
+        MessageBox.Show(
+          "Executabilul nu este denumit \"ArhivUtility.exe\".\n" +
+          "Redenumiti fisierul si reincercati pentru a putea primi actualizari automate.",
+          "Actualizare imposibila",
+          MessageBoxButtons.OK,
+          MessageBoxIcon.Warning);
+        return;
+      }
+
       // update checker
       AutoUpdater.RunUpdateAsAdmin = false;
       AutoUpdater.Mandatory = true;
